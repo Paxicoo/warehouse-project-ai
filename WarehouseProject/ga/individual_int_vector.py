b7717d6e4da953554cea96d9f7699822b7dd2204
Francisco Rosa <francisco.panchico@gmail.com>
2023-04-24 13:38:40 +0100

    comentarios relevantes da professora

--- WarehouseProject/ga/individual_int_vector.py
+++ WarehouseProject/ga/individual_int_vector.py
@@ -9,6 +9,22 @@ class IntVectorIndividual(Individual):
         super().__init__(problem, num_genes)
         # TODO
 
+        # depois disso, antes de tudo é criar os individuos
+        # preencher o genoma na classe individual
+
+        # individuos têm inteiros, os inteiros correspondem aos produtos
+        # e a ordem do genoma é a ordem pela qual os produtos são apanhados
+        # no final vai para a porta
+
+        #o objetivo é criar um array de inteiros, com os números dos produtos não repetidos !!
+
+        # na solução do algoritmo genético, vejo onde está o par (cell1 é o agent, cell2 é o inteiro do genoma)
+        # por exemplo, no indivíduo: 1-2-3 calcula-se do agente ao produto 1, do produto 1 ao 2, do 2 ao 3, e do 3 à porta
+
+        # o fitness desse indivíduo é a soma dos custos de cada par do genoma
+
+        # depois é fazer o compute_fitness, é facil
+
     def swap_genes(self, other, index: int):
         aux = self.genome[index]
         self.genome[index] = other.genome[index]
