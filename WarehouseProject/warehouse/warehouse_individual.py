d15ba04f64a67b3fb5ed12819f493a6c9e6797b9
Paxicoo <francisco.panchico@gmail.com>
2023-05-10 11:12:07 +0100

    compute fitness with multiple agents done

--- WarehouseProject/warehouse/warehouse_individual.py
+++ WarehouseProject/warehouse/warehouse_individual.py
@@ -8,7 +8,6 @@ class WarehouseIndividual(IntVectorIndividual):
         # TODO
 
     def compute_fitness(self) -> float:
-        # TODO
         # o fitness é a soma dos custos de cada par do genoma
         # o genoma é um array de inteiros, que representa a ordem pela qual o agente vai passar pelas celulas
 
@@ -21,40 +20,76 @@ class WarehouseIndividual(IntVectorIndividual):
         # fazer print do fitness para ver se está tudo bem
         # print(self.fitness)
 
-        # TODO FAZER PARA VÁRIOS AGENTES
-
         self.fitness = 0
         agent_search = self.problem.agent_search
-
-        # Calcular a distância do agente ao primeiro produto no genoma
+        # Número de produtos
+        product_count = len(agent_search.products)
+        # Último produto visitado
+        last_product_cell = None
+        # A menos que o genoma comece com um agente, o primeiro agente é o primeiro a pegar um produto
         agent_cell = agent_search.forklifts[0]
-        first_product_cell = agent_search.products[self.genome[0] - 1]
-        distance_agent_to_first_product = agent_search.distances.get(((agent_cell.line, agent_cell.column),
-                                                                      (first_product_cell.line,
-                                                                       first_product_cell.column)),
-                                                                     float('inf'))
-        self.fitness += distance_agent_to_first_product
+        # A saída é sempre a mesma
+        exit_cell = agent_search.exit
 
-        # Calcular a distância para cada par de produtos adjacentes no genoma
-        for i, product1_index in enumerate(self.genome[:-1]):
-            product1_cell = agent_search.products[product1_index - 1]
-            product2_cell = agent_search.products[self.genome[i + 1] - 1]
-            distance = agent_search.distances.get(((product1_cell.line, product1_cell.column),
-                                                   (product2_cell.line, product2_cell.column)), float('inf'))
+        # Vamos percorrer o genoma
+        for i, genome_value in enumerate(self.genome):
+            # Verificar se o valor do genoma corresponde a um agente
+            if genome_value > product_count:
+                # Se houver um último produto visitado, calcular a distância do último produto à saída
+                if last_product_cell is not None:
+                    distance_last_product_to_exit = agent_search.distances.get(
+                        ((last_product_cell.line, last_product_cell.column),
+                         (exit_cell.line, exit_cell.column)), float('inf'))
+                    self.fitness += distance_last_product_to_exit
+                    last_product_cell = None
+                # Se não houver um último produto visitado, significa que o agente não vai pegar nenhum produto
+                # então vamos calcular a distância do agente à saída
+                else:
+                    distance_agent_to_exit = agent_search.distances.get(
+                        ((agent_cell.line, agent_cell.column),
+                         (exit_cell.line, exit_cell.column)), float('inf'))
+                    self.fitness += distance_agent_to_exit
 
-            # Se a distância for infinita, quer dizer que acedemos ao dicionário com a chave errada (ordem contrária)
-            if distance == float('inf'):
-                distance = agent_search.distances.get(((product2_cell.line, product2_cell.column),
-                                                       (product1_cell.line, product1_cell.column)), float('inf'))
+                # Vamos mudar para o agente correto com base no valor do genoma
+                # genome_value - product_count é o index do agente na matriz de forklifts
+                agent_cell = agent_search.forklifts[genome_value - product_count]
+            # Se o valor do genoma corresponder a um produto
+            else:
+                # Calcular a distância do agente (ou do último produto) ao produto atual
+                product_cell = agent_search.products[genome_value - 1]
+                # Se o agente ainda não pegou nenhum produto
+                if last_product_cell is None:
+                    # O agente vai para o primeiro produto
+                    distance = agent_search.distances.get(((agent_cell.line, agent_cell.column),
+                                                           (product_cell.line, product_cell.column)), float('inf'))
+                # Se o agente já pegou pelo menos um produto
+                else:
+                    # O agente vai do último produto ao produto atual
+                    distance = agent_search.distances.get(((last_product_cell.line, last_product_cell.column),
+                                                           (product_cell.line, product_cell.column)), float('inf'))
 
-            self.fitness += distance
+                    # Se a distância for infinita, significa que acedemos ao dicionário com a ordem errada da chave
+                    if distance == float('inf'):
+                        distance = agent_search.distances.get(((product_cell.line, product_cell.column),
+                                                               (last_product_cell.line, last_product_cell.column)),
+                                                              float('inf'))
 
-        # Calcular a distância do último produto no genoma à porta
-        last_product_cell = agent_search.products[self.genome[-1] - 1]
-        exit_cell = self.problem.agent_search.exit
-        distance_last_product_to_exit = agent_search.distances.get(((last_product_cell.line, last_product_cell.column),
-                                                                    (exit_cell.line, exit_cell.column)), float('inf'))
-        self.fitness += distance_last_product_to_exit
+                self.fitness += distance
+                # O último produto visitado passa a ser o produto atual
+                last_product_cell = product_cell
+
+        # Se houver um último produto visitado, calcular a distância do último produto à saída
+        if last_product_cell is not None:
+            distance_last_product_to_exit = agent_search.distances.get(
+                ((last_product_cell.line, last_product_cell.column),
+                 (exit_cell.line, exit_cell.column)), float('inf'))
+            self.fitness += distance_last_product_to_exit
+        # Se não houver um último produto visitado, calcular a distância do agente à saída
+        else:
+            distance_agent_to_exit = agent_search.distances.get(
+                ((agent_cell.line, agent_cell.column),
+                 (exit_cell.line, exit_cell.column)), float('inf'))
+            self.fitness += distance_agent_to_exit
 
         return self.fitness
 
